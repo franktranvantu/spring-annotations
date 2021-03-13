e6af99fb72a48477091fa1e9bd5a8aa462cf4ed5
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-13 17:00:25 +0700

    Update

--- src/main/java/com/franktran/Application.java
+++ src/main/java/com/franktran/Application.java
@@ -2,8 +2,12 @@ package com.franktran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class Application {
 
   public static void main(String[] args) {
