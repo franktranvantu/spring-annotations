b9406b50c91a006d15a22819e7431be5f40377ac
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-13 18:13:30 +0700

    Update

--- src/main/java/com/franktran/controller/HelloController.java
+++ src/main/java/com/franktran/controller/HelloController.java
@@ -1,8 +1,20 @@
 package com.franktran.controller;
 
+import com.franktran.service.HelloService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
+  private final HelloService service;
+
+  public HelloController(HelloService service) {
+    this.service = service;
+  }
+
+  @RequestMapping()
+  public String hello() {
+    return service.hello();
+  }
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>@Service</title>
+    </head>
+    <body>
+        <h1>Welcome to @Service</h1>
+    </body>
+</html>
